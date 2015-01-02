369484a844bb4e6660943d50cd1c9e8e8695a312
Wade Clinton <wade.clinton@gmail.com>
2015-01-02 12:13:26 -0800

    Update stats formatting

--- src/main/java/com/ppa8ball/stats/PlayerStat.java
+++ src/main/java/com/ppa8ball/stats/PlayerStat.java
@@ -2,7 +2,9 @@ package com.ppa8ball.stats;
 
 import com.googlecode.objectify.annotation.Entity;
 import com.googlecode.objectify.annotation.Id;
+import com.googlecode.objectify.annotation.Ignore;
 import com.googlecode.objectify.annotation.Index;
+import com.googlecode.objectify.annotation.OnLoad;
 import com.googlecode.objectify.annotation.Unindex;
 
 @Entity
@@ -22,8 +24,23 @@ public class PlayerStat {
 	@Unindex public double actualAverage;
 	@Unindex public int perfectNights;
 	
+	@Ignore public double displayAdjustedAverage;
+	@Ignore public double displayActualAverage;
+	
+
 	public PlayerStat()
 	{
-		
 	}
+	
+	 @OnLoad void onLoad() 
+	 {
+		 displayActualAverage = RoundTo1Decimals(actualAverage);
+		 displayAdjustedAverage = RoundTo1Decimals(adjustedAverage);
+	 }
+	 
+	 private static double RoundTo1Decimals(double d)
+	 {
+		 double rounded = Math.round(10* d);
+		 return rounded / 10;
+	 }
 }
