5ae07b84a760de8e21ce6a5cbd6286cb8530f913
Wade Clinton <wade.clinton@gmail.com>
2015-01-24 13:14:08 -0800

    Added table 1 & 2 to the match entity

--- src/main/java/com/ppa8ball/schedule/Load.java
+++ src/main/java/com/ppa8ball/schedule/Load.java
@@ -17,16 +17,14 @@ import com.ppa8ball.stats.CellHelp;
 
 public class Load
 {
-
 	private final String PPAUrl = "http://www.ppa8ball.com";
 
 	private final String FirstHalfSchedule = PPAUrl + "/schedule/2014-2015_First_Half_Schedule.xls";
 	private final String SecondHalfSchedule = PPAUrl + "/schedule/2014-2015_Second_Half_Schedule.xls";
-	
+
 	private final int FirstHalfYear = 2014;
 	private final int SecondHalfYear = 2015;
 
-	
 	public Schedule LoadFromExcel()
 	{
 		try
@@ -35,19 +33,19 @@ public class Load
 
 			Sheet sheet = workbook.getSheet(0);
 
-			Schedule firstHalf = getSchedule(sheet,FirstHalfYear);
-			
+			Schedule firstHalf = getSchedule(sheet, FirstHalfYear);
+
 			workbook = Workbook.getWorkbook(getExcelSpreadSheet(SecondHalfSchedule));
-			
+
 			sheet = workbook.getSheet(0);
-			
-			Schedule secondHalf = getSchedule(sheet,SecondHalfYear);
-			
+
+			Schedule secondHalf = getSchedule(sheet, SecondHalfYear);
+
 			for (Week week : secondHalf.weeks)
 			{
 				firstHalf.weeks.add(week);
 			}
-			
+
 			return firstHalf;
 
 		} catch (BiffException e)
@@ -59,69 +57,92 @@ public class Load
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-		
+
 		return null;
 	}
-	
+
 	private Schedule getSchedule(Sheet sheet, int year)
 	{
 		Cell startCell = sheet.getCell("B1");
-		
+
 		Cell cell = startCell;
-		
+
 		Schedule schedule = new Schedule();
-		
+
 		while (schedule.weeks.size() < 9)
 		{
 			Week week = getWeek(sheet, cell);
-			
-			if (week != null){
-				
-				//Match match = null;
-				
-				while(week.getMatches().size() <5)
+
+			if (week != null)
+			{
+
+				// Match match = null;
+
+				while (week.getMatches().size() < 5)
 				{
 					cell = CellHelp.getCellBelow(sheet, cell);
-					
+
 					Match match = getMatch(cell);
-					
-					
+
 					if (match != null)
 					{
 						match.week = week.number;
 						match.season = week.season;
 						match.match = week.getMatches().size();
 						week.getMatches().add(match);
+						
+						cell = CellHelp.getCellBelow(sheet, cell);
+						
+						String [] tables = getTables(cell);
+						
+						match.table1 = tables[0];
+						match.table2 = tables[1];
+						
+						
 					}
 				}
-				
-				week.date.setYear(year-1900);
+
+				week.date.setYear(year - 1900);
 				schedule.weeks.add(week);
-				
-				
-			}			
+
+			}
 			cell = CellHelp.getNextTopColumn(sheet, cell);
 		}
-		
+
 		return schedule;
 	}
-	
+
 	private Match getMatch(Cell cell)
 	{
 		String contents = cell.getContents();
-		
+
 		if (contents.contains("at"))
 		{
-			Match match =new Match();
-			String [] teams = contents.split("at");
-			
+			Match match = new Match();
+			String[] teams = contents.split("at");
+
 			match.homeTeam = Integer.parseInt(teams[1].trim());
 			match.awayTeam = Integer.parseInt(teams[0].trim());
-			
-			
+
 			return match;
 		}
-		
+
+		return null;
+	}
+
+	private String [] getTables(Cell cell)
+	{
+		String contents = cell.getContents();
+
+		if (contents.contains("Table"))
+		{
+			String [] tables = contents.substring("Tables ".length()).split("&");
+			
+			tables[0] = tables[0].trim();
+			tables[1] = tables[1].trim();
+			
+			return tables;
+		}
 		return null;
 	}
 
@@ -142,55 +163,51 @@ public class Load
 		}
 		return null;
 	}
-	
+
 	private int getWeekDate(Cell cell)
 	{
 		String contents = cell.getContents();
-		
+
 		if (contents == null || !contents.startsWith("Week"))
 			return -1;
-		
+
 		String weekString = contents.substring(5);
-		
+
 		int week = Integer.parseInt(weekString);
-		
-		return  week;
+
+		return week;
 	}
-	
+
 	private Week getWeek(Sheet sheet, Cell cell)
 	{
 		Week week = null;
 		Date date = null;
-		
-		for (int i=0;i<20;i++)
+
+		for (int i = 0; i < 20; i++)
 		{
-			//look for a column with dates
-			
+			// look for a column with dates
+
 			date = getDate(cell);
-			
+
 			if (date != null)
 				break;
-			
+
 			cell = CellHelp.getCellBelow(sheet, cell);
 		}
-		
+
 		if (date != null)
 		{
 			Cell weekCell = CellHelp.getCellBelow(sheet, cell);
-			
+
 			int weekNumber = getWeekDate(weekCell);
-			
-			if (weekNumber >=0)
+
+			if (weekNumber >= 0)
 			{
-				week = new Week("",weekNumber,date);
+				week = new Week("", weekNumber, date);
 			}
 		}
 		return week;
 	}
-
-	
-	
-	
 
 	private InputStream getExcelSpreadSheet(String url)
 	{
--- src/main/java/com/ppa8ball/schedule/Match.java
+++ src/main/java/com/ppa8ball/schedule/Match.java
@@ -3,24 +3,34 @@ package com.ppa8ball.schedule;
 import com.googlecode.objectify.annotation.Entity;
 import com.googlecode.objectify.annotation.Id;
 import com.googlecode.objectify.annotation.Index;
+
 @Entity
 public class Match
 {
-	@Id public Long id;
-	@Index public String season;
-	@Index public int week;
-	@Index public int match;
-	@Index public int homeTeam;
-	@Index public int awayTeam;
-	
-	
+	@Id
+	public Long id;
+	@Index
+	public String season;
+	@Index
+	public int week;
+	@Index
+	public int match;
+	@Index
+	public int homeTeam;
+	@Index
+	public int awayTeam;
+	@Index
+	public String table1;
+	@Index
+	public String table2;
+
 	public Match()
 	{
-		
+
 	}
-	
+
 	public String toString()
 	{
-		return  awayTeam + " at " + homeTeam;
+		return awayTeam + " at " + homeTeam + " tables: " + table1 + " & " + table2;
 	}
 }
