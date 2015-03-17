22e73c7ee461a63927d127f42ceefde535e3c204
Wade Clinton <wade.clinton@gmail.com>
2015-03-17 12:56:28 -0700

    Cleaned up the hibernate configuration

--- src/main/java/com/ppa8ball/util/HibernateUtil.java
+++ src/main/java/com/ppa8ball/util/HibernateUtil.java
@@ -14,26 +14,30 @@ public class HibernateUtil
 	{
 		try
 		{
+			URI dbUri = new URI(System.getenv("DATABASE_URL"));
+			String username = dbUri.getUserInfo().split(":")[0];
+			String password = dbUri.getUserInfo().split(":")[1];
+			String dbUrl = "jdbc:postgresql://" + dbUri.getHost() + ":" + dbUri.getPort() + dbUri.getPath();
+			dbUrl += "?user=" + username;
+			dbUrl += "&password=" + password;
 
-//			URI dbUri = new URI(System.getenv("DATABASE_URL"));
-//			String username = dbUri.getUserInfo().split(":")[0];
-//			String password = dbUri.getUserInfo().split(":")[1];
-//			String dbUrl = "jdbc:postgresql://" + dbUri.getHost() + ":" + dbUri.getPort() + dbUri.getPath();
-//			
-		//	dbUrl += "?ssl=true&sslfactory=org.postgresql.ssl.NonValidatingFactory";
-
+			//only add the ssl if we are running in production.
+			if (!dbUri.getHost().contains("localhost"))
+			{
+				dbUrl += "&ssl=true&sslfactory=org.postgresql.ssl.NonValidatingFactory";
+			}
 			/** Load the hibernate.cfg.xml from the classpath **/
 			Configuration cfg = new Configuration();
-			
+
 			cfg.configure();
-//			cfg.setProperty("hibernate.connection.url", dbUrl);
-//			cfg.setProperty("connection.username", username);
-//			cfg.setProperty("connection.password", password);
-			
-			cfg.setProperty("hibernate.connection.requireSSL","true");
+			cfg.setProperty("hibernate.connection.url", dbUrl);
+			cfg.setProperty("connection.username", username);
+			cfg.setProperty("connection.password", password);
+
+			cfg.setProperty("hibernate.connection.requireSSL", "true");
 
 			SessionFactory sessionFactory = cfg.buildSessionFactory();
-			
+
 			return sessionFactory;
 
 		} catch (Throwable ex)
